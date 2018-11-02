ec12adf8e27135fbdda29b2fc299888f5175953e
Jonathan Johnson <me@jondavidjohn.com>
2018-11-02 09:50:16 -0500

    Revert "test broken access token"
    This reverts commit 0641868ddf5a9ecc966360484bd65bb82e74ff33.

--- ci/deploy_preview.sh
+++ ci/deploy_preview.sh
@@ -4,7 +4,6 @@ set -eu
 
 repo="cloudbees/shipyard"
 user_access_token=${GITHUB_DEPLOY_USER_ACCESS_TOKEN:?"Missing GITHUB_DEPLOY_USER_ACCESS_TOKEN environment variable"}
-user_access_token="notreal"
 if ! branch=$(git symbolic-ref --short HEAD 2>/dev/null); then
   branch=${CI_BRANCH:?"Could not read branch from either local checkout nor environment variable CI_BRANCH"}
 fi
@@ -20,17 +19,17 @@ if ! deployment=$(curl -s \
                   -d "{ \"ref\": \"${branch}\", \"environment\": \"preview\", \"description\": \"Styleguide Preview\", \"transient_environment\": true, \"auto_merge\": false, \"required_contexts\": []}" \
                   -H "Content-Type: application/json" \
                   "https://api.github.com/repos/${repo}/deployments"); then
-  echo "POSTing deployment status failed, exiting" 1>&2
+  echo "POSTing deployment status failed, exiting (not failing build)" 1>&2
   exit 1
 fi
 
 if ! deployment_id=$(echo "${deployment}" | jq '.id'); then
-  echo "Could not extract deployment ID from API response, exiting"
+  echo "Could not extract deployment ID from API response, exiting (not failing build)"
   exit 2
 fi
 
 if ! surge ./_site/ "${surge}"; then
-  echo "Deployment of the preview failed, exiting" 1>&2
+  echo "Deployment of the preview failed, exiting (not failing build)" 1>&2
   exit 3
 fi
 
@@ -41,6 +40,6 @@ if ! curl -s \
   -H "Content-Type: application/json" \
   "https://api.github.com/repos/${repo}/deployments/${deployment_id}/statuses" \
   > /dev/null ; then
-  echo "POSTing deployment status failed, exiting" 1>&2
+  echo "POSTing deployment status failed, exiting (not failing build)" 1>&2
   exit 4
 fi
