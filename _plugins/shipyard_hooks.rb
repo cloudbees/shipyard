37d24f9c6e9c093b37c75dc3924704151071abe4
Ryan Wilke <ryanwilke@gmail.com>
2018-12-11 14:27:30 +0100

    Turn the hooks on again

--- _plugins/shipyard_hooks.rb
+++ _plugins/shipyard_hooks.rb
@@ -1,7 +1,10 @@
-# Jekyll::Hooks.register :site, :after_init do
-#   system 'cd _assets/shipyard && yarn install'
-# end
-
-# Jekyll::Hooks.register :site, :pre_render do
-#   system 'cd _assets/shipyard && yarn build:dist'
-# end
+Jekyll::Hooks.register :site, :after_init do
+  system %(
+    cd _assets/shipyard &&
+    yarn install &&
+    yarn build:dist
+  )
+  unless ENV['JEKYLL_ENV'].nil?
+    system %(chown -f -R jekyll:jekyll ./)
+  end
+end
