82a5e77e179ef83a8486c0ace9cc92cb70f37bdf
Ryan Wilke <ryanwilke@gmail.com>
2018-12-11 11:50:07 +0100

    try fixing the errors

--- _script/ci/jekyll.test.sh
+++ _script/ci/jekyll.test.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -e # halt script on error
 
-chown -R jekyll:jekyll ./
+pwd
+(
+  cd _assets/shipyard/;
+  yarn install;
+  yarn build:dist;
+  chown -R jekyll:jekyll ./
+)
+
 JEKYLL_ENV=test jekyll build --trace --destination _site
